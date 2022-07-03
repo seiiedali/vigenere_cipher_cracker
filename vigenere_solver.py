9133c1ca2382415b2ff3970bc6349b854c2507b4
seiiedali <ali.miramirkhani@gmail.com>
2022-07-03 10:30:37 +0430

    final-touch

--- vigenere_solver.py
+++ vigenere_solver.py
@@ -1,9 +1,8 @@
 # Imports
+import string
 
 # Global variables
 max_key_lenght = 10
-
-english_IOC = 0.065
 
 english_frequences = [
     0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015,
@@ -12,8 +11,6 @@ english_frequences = [
     0.00978, 0.02360, 0.00150, 0.01974, 0.00074]
 
 # String Cleaner
-
-
 def string_cleaner(string):
     clean_string = string.upper()
     # Filter only A-Z
@@ -21,32 +18,25 @@ def string_cleaner(string):
         [char for char in clean_string if 65 <= ord(char) <= 90])
     return clean_string
 
-# IOC calculator
-
-
+# IC calculator
 def ic_calculator(text):
     num_letters = [0] * 26
     text_lenght = len(text)
     for char in text:
         num_letters[ord(char) - 65] += 1
-
     ic = sum([entry ** 2 for entry in num_letters])
     ic /= text_lenght ** 2
     return ic
 
-
-# Key lenght
+# Key lenght finder
 def key_length_finder(cipher_text):
-    # Cipher cleaning
-    clean_cipher_text = cipher_text.upper()
-    clean_cipher_text = ''.join(clean_cipher_text.split())
 
     # Creating sub collections
     sub_strings_dictionary = {}
     for i in range(1, max_key_lenght+1, 1):
         temp_list = []
         for j in range(0, i, 1):
-            temp_list.append(clean_cipher_text[j::i])
+            temp_list.append(cipher_text[j::i])
         sub_strings_dictionary[i] = temp_list
 
     # finding the highest coincidence
@@ -57,7 +47,7 @@ def key_length_finder(cipher_text):
             temp_list.append(ic_calculator(sub_string))
         ioc_dictionaray[key] = temp_list
 
-    # selecting the key
+    # selecting the key length
     ave_ioc_dict = {}
     max_ioc = 0.0
     final_key_lenght = 0
@@ -66,7 +56,7 @@ def key_length_finder(cipher_text):
         if float(ave_ioc_dict[key]) > max_ioc:
             max_ioc = ave_ioc_dict[key]
             final_key_lenght = key
-    return final_key_lenght, sub_strings_dictionary[final_key_lenght]
+    return final_key_lenght, sub_strings_dictionary[final_key_lenght], ioc_dictionaray
 
 
 # Shift each character of a string
@@ -79,7 +69,6 @@ def mg_sum(shifted_text):
     num_letters = [0] * 26
     for char in shifted_text:
         num_letters[ord(char) - 65] += 1
-
     text_length = len(shifted_text)
     sub_string_letter_fre = []
     for letter in num_letters:
@@ -87,29 +76,30 @@ def mg_sum(shifted_text):
     total_mg = 0
     for index, char in enumerate(sub_string_letter_fre):
         total_mg += sub_string_letter_fre[index] * english_frequences[index]
-
-    # print(total_mg)
     return total_mg
 
 
 # Key Cracker
-def key_solver(cipher_text, sub_strings, key_length):
-    # finding the key
+def key_solver(sub_strings):
     key_shift = {}
+    mg_dict = {}
     for index, string in enumerate(sub_strings):
         max_mg = 0
+        temp_list = []
         for shift in range(1, 26, 1):
             shifted = shift_text(shift, string)
             mg = mg_sum(shifted)
+            temp_list.append(round(mg,3))
             if mg > max_mg:
                 max_mg = mg
                 key_shift[index+1] = shift
+        mg_dict[index+1] = temp_list
     deciphering_key = []
     enciphering_key = []
     for key, num in key_shift.items():
         deciphering_key.append(chr(num + 65))
         enciphering_key.append(chr(26 - num + 65))
-    return (deciphering_key, enciphering_key)
+    return deciphering_key, enciphering_key, key_shift, mg_dict
 
 
 # Decryptor
@@ -123,6 +113,21 @@ def vigenere_decryptor(cipher_text, deciphering_key, key_length):
         plain_text.append(decrypted_char)
     return plain_text
 
+def plain_text_formatter(cipher_text, plain_text):
+    formatted_plain_text = []
+    plain_counter = 0
+    for char in cipher_text:
+        if char not in string.ascii_letters:
+            formatted_plain_text.append(char)
+        elif char in string.ascii_lowercase:
+            formatted_plain_text.append(plain_text[plain_counter].lower())
+            plain_counter += 1
+        else:
+            formatted_plain_text.append(plain_text[plain_counter].upper())
+            plain_counter += 1
+
+    return formatted_plain_text
+
 
 # Main function
 def main():
@@ -133,16 +138,26 @@ def main():
 
     # function calls
     clean_cipher = string_cleaner(cipher_text)
-    key_length, sub_strings = key_length_finder(clean_cipher)
-    deciphering_key, enciphering_key = key_solver(
-        clean_cipher, sub_strings, key_length)
+    key_length, sub_strings, key_ioc_dictionary = key_length_finder(clean_cipher)
+    deciphering_key, enciphering_key, key_shift, mg_dict = key_solver(sub_strings)
     plain_text = vigenere_decryptor(clean_cipher, deciphering_key, key_length)
+    formatted_plain_text = plain_text_formatter(cipher_text, plain_text)
 
     # printing the resaults
     print("The Key length is", key_length)
     print("The Enciphering key is", enciphering_key)
     print("The Deciphering key is", deciphering_key)
     print("The Plain text is \n", ''.join(plain_text))
+    print("The Formatted Plain text is \n", ''.join(formatted_plain_text))
+
+
+    print("The Key IOC dictionary is \n", key_ioc_dictionary)
+    for key, value in key_ioc_dictionary.items():
+        print("For the key length " , key, " the average is:", (sum(value)/len(value)))
+
+    print("The Tested Shifts are \n", mg_dict)
+    print("The Key shifts are \n", key_shift)
+    print("The valid sub strings are \n", sub_strings)
 
 
 if __name__ == '__main__':
